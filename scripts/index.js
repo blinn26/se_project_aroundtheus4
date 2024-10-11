4f800ab5f9943702d59cebf0673f05730cfbdca7
Ben Linn <benlinn26@gmail.com>
2024-10-11 09:47:55 -0700

    new card images and new profile image

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,12 @@
 /* -------------------------------------------------------------------------- */
 
 const initialCards = [
-  { name: 'Yosemite Valley', link: './images/yosemite.jpg' },
-  { name: 'Lake Louise', link: './images/lake-louise.jpg' },
-  { name: 'Bald Mountains', link: './images/bald-mountains.jpg' },
-  { name: 'Latemar', link: './images/latemar.jpg' },
-  { name: 'Vanoise National Park', link: './images/vanoise.jpg' },
-  { name: 'Lago di Braies', link: './images/lago.jpg' },
+  { name: 'San Fran', link: './images/sanfran.jpg' },
+  { name: 'Washington DC', link: './images/washingtondc.jpg' },
+  { name: 'New York', link: './images/newyork.jpg' },
+  { name: 'Cape Cod', link: './images/capecod.jpg' },
+  { name: 'Santa Monica', link: './images/california.jpg' },
+  { name: 'Colorado', link: './images/colorado.jpg' },
 ];
 
 /* -------------------------------------------------------------------------- */
